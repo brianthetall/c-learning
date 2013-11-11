5a14c32bf2c33dff140b16a47d404d2f970f5d1e
Brian Wagner <brianthetall@gmail.com>
2013-11-11 14:03:53 -0500

    Basic toString function written for Motor-Struct; need to make memory allocation more efficient.

--- inductionMotor.c
+++ inductionMotor.c
@@ -16,23 +16,49 @@ struct motor{
   double lengthCoil;
   double resistance;
   double u;//magnetic permeability of material
+  //add function pointer to toString()
 };
+
+char* motorString(char *string, struct motor *m){
+  //  char *retval=(char*)malloc(sizeof(char;
+  if(m==NULL)
+    return NULL;
+
+  sprintf(string,"Phases: %d\r\n",(*m).phases);
+  //  sprintf(string
+  printf("Sizeof(*m)=%d\r\nSizeof(m)=%d\r\n",sizeof(*m),sizeof(m));
+
+  return string;
+}
 
 int main(int argc,char *argv[]){
 
   struct motor m;
-  
-  for(int i=0;i<argc;i++)
-    printf("%d=%s\r\n",i,argv[i]);
   
   if(argc==7){
     m.phases = atoi(argv[1]);
   }else{
     printf("Phases: ");
     scanf("%d",&m.phases);
+    printf("Poles: ");
+    scanf("%d",&m.poles);
+    printf("Turns: ");
+    scanf("%d",&m.turns);
+    printf("Surface Area of Coil: ");
+    scanf("%lf",&m.surfaceAreaCoil);
+    printf("Length of Coil: ");
+    scanf("%lf",&m.lengthCoil);
+    printf("Resistance of Coil: ");
+    scanf("%lf",&m.resistance);
+    printf("Permeability: ");
+    scanf("%lf",&m.u);
   }
   
   printf("Phases: %d\r\n",m.phases);
+
+  char *string=(char*)malloc(256*sizeof(char));
+  motorString(string,&m);
+  printf("%s\r\n",string);
   
   return 0;
 }
